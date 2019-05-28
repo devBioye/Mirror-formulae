05ee8971eb599b2e10057b82a716179ff7006e31
devBioye <44414468+devBioye@users.noreply.github.com>
2019-05-28 07:54:59 -0800

    Add files via upload

---
+++ Mirror-formulae.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mirror_formulae
+{
+    class Mirror_formulaeCLI
+    {
+        public int ObjectDist { get; set; }
+        public int FocalDist { get; set; }
+        public int ImageDist { get; set; }
+        public int ImageDistance(string MirrorType, double ObjectDistance, double FocalLength)
+        {
+           //Using Real is Positive.....
+            if (MirrorType.ToLower() == "concave")
+            {
+                ObjectDist = (1 / ObjectDistance);
+                FocalDist = (1 / FocalLength);
+                ImageDist = 1/( FocalDist - ObjectDist);
+                
+            }
+            //Using Real is Positive
+            else if (MirrorType.ToLower() == "convex")
+            {
+                ObjectDist = (1 / ObjectDistance);
+                FocalDist = (1 / FocalLength);
+                ImageDist = 1 / (-FocalDist - ObjectDist);
+
+            }
+            else {
+                throw new Exception("This is not a MirrorType.Check it and try again");
+            
+            }
+            return ImageDist;
+        }
+        
+    }
+}
